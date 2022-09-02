964158483c7d321f929934475c95aa860524f4ab
Wolkensteine <bokaj.bb22@gmail.com>
2022-09-02 08:30:53 +0200

    Adding Variables and constants

---
+++ src/Main/Unterricht/TicTacToe/Utils/Constants.java
@@ -0,0 +1,10 @@
+package Main.Unterricht.TicTacToe.Utils;
+
+import java.awt.Color;
+
+public class Constants {
+
+	public static Color PrimaryColor;
+	public static Color SecondaryColor;
+	
+}
---
+++ src/Main/Unterricht/TicTacToe/Utils/Variables.java
@@ -0,0 +1,12 @@
+package Main.Unterricht.TicTacToe.Utils;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Variables {
+
+	// MainWindow
+	public static JFrame MainWindow;
+	public static JPanel MainPanel;
+	
+}
