77ec1de88c880e93d2dbbadeef792cb14007b932
Wolkensteine <bokaj.bb22@gmail.com>
2022-09-02 15:31:18 +0200

    Edited Windowclass

--- src/Main/Unterricht/TicTacToe/Graphics/MainWindow.java
+++ src/Main/Unterricht/TicTacToe/Graphics/MainWindow.java
@@ -1,18 +1,18 @@
 package Main.Unterricht.TicTacToe.Graphics;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
 	
-	public MainWindow (String name, int width, int height, boolean visibleOnStartup) {
-		
+	// Just a small class to make a window with attributes directly in the initialization
+	public MainWindow (String name, int width, int height, boolean visibleOnStartup, Color BackgroundColor) {
 		this.setName(name);
-		
 		this.setSize(width, height);
-		
+		this.getContentPane().setBackground(BackgroundColor);
 		this.setVisible(visibleOnStartup);
-		
 	}
 	
 }
