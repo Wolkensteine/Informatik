49d187bf830921772fe5e4694d856357653f68dc
hendrikridder <hendrikridder@localhost>
2022-10-13 10:49:35 +0200

    Firebase

--- src/Main/Unterricht/TicTacToe/Logic/Server/Online/FirebaseInitialization.java
+++ src/Main/Unterricht/TicTacToe/Logic/Server/Online/FirebaseInitialization.java
@@ -2,6 +2,20 @@ package Main.Unterricht.TicTacToe.Logic.Server.Online;
 
 import java.io.FileInputStream;
 
+import org.springframwork.stereotype.Service;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOption;
+
+@Service
 public class FirebaseInitialization {
-	FileInputStream 
+	FileInputStream serviceAccount =
+			  new FileInputStream("./ServiceAcount.json");
+
+	FirebaseOptions options = new FirebaseOptions.Builder()
+	  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+	  .setDatabaseUrl("https://tik-tak-toe-d7352.firebaseio.com")
+	  .build();
+
+	FirebaseApp.initializeApp(options);
 }
