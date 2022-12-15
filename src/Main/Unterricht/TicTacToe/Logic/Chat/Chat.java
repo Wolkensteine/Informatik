1557492276e474617e7bf69dcae992a4ebaba3e4
Wolkensteine <Wolkenstein06@gmail.com>
2022-12-15 10:39:57 +0100

    Chat functions reformat

--- src/Main/Unterricht/TicTacToe/Logic/Chat/Chat.java
+++ src/Main/Unterricht/TicTacToe/Logic/Chat/Chat.java
@@ -6,34 +6,22 @@ public class Chat {
 
 	// Will format the chat
 	public void FormatChat (String OpponentPlayerName) {
-		
 		Variables.ChatTextArea.setText("Chat with: " + OpponentPlayerName);
-		
 	}
 	
 	// Will clear chat
-	public void ChatClear () {
-		
+	public void ChatClear () {		
 		Variables.ChatTextArea.setText("");
-		
-	}
-	
-	public void HideChat () {
-		
 	}
 	
 	// Will add message from opponent to chat
 	public void addReceivedMessage (String Message) {
-		
 		Variables.ChatTextArea.setText(Variables.ChatTextArea.getText() + Message);
-		
 	}
 	
 	// Will add own send message to chat
 	public void addSendMessage (String Message) {
-		
 		Variables.ChatTextArea.setText(Variables.ChatTextArea.getText() + Message);
-		
 	}
 
 }
