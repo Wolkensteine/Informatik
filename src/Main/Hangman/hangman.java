cb0ba8c2b295378abf6af5bcded3aacd6075436c
Wolkensteine <bokaj.bb22@gmail.com>
2022-12-15 17:53:11 +0100

    First setup of hangman game

--- src/Main/Hangman/hangman.java
+++ src/Main/Hangman/hangman.java
@@ -1,8 +1,34 @@
 package Main.Hangman;
 
+import java.util.Scanner;
+
 public class hangman {
 
+	public static String Endmessage;
+	
+	public static Scanner InputScanner = new Scanner(System.in);
+	
+	public static String Word;
+	
 	public static void main(String[] args) {
+		
+		System.out.println("Starting hangman game ...");
+		
+		System.out.println("Player ome give me a word!");
+		
+		Word = InputScanner.next();
+		
+		System.out.println(Word);
+		
+		while (true) {
+			
+			if (true) {
+				break;
+			}
+			
+		}
+		
+		System.out.println("Hangman game done. You " + Endmessage);
 		
 	}
 
