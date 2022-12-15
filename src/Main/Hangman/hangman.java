564e0ee70cad2831b56d83a9a57608faa9680be8
Wolkensteine <bokaj.bb22@gmail.com>
2022-12-15 18:04:59 +0100

    Adding first logic

--- src/Main/Hangman/hangman.java
+++ src/Main/Hangman/hangman.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class hangman {
 
+	// Set variables
 	public static String Endmessage;
-	
 	public static Scanner InputScanner = new Scanner(System.in);
-	
 	public static String Word;
+	public static char[] WordLetters;
+	public static char[] DifferentLettersInWord;
 	
 	public static void main(String[] args) {
 		
@@ -16,14 +17,26 @@ public class hangman {
 		
 		System.out.println("Player ome give me a word!");
 		
-		Word = InputScanner.next();
+		Word = InputScanner.next(); // Get next input and store it
 		
-		System.out.println(Word);
+		WordLetters = new char[Word.length()]; // Make the letter array as long as the length of the input String
 		
+		// Get different chars in the word to later check, if they are inputed to find them
+		for (int i = 0; i < Word.length(); i++) {
+			
+		}
+		
+		// Begin game
 		while (true) {
 			
-			if (true) {
+			if () { // Check if you won
+				Endmessage = "won! Congratulations!"; // Change the message at the end
 				break;
+			} else if () { // Check if you lost
+				Endmessage = "lost ... Maybe you'll get it next time"; // Change the message at the end
+				break;
+			} else { // Do normal
+				
 			}
 			
 		}
